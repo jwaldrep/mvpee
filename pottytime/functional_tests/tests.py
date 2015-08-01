bea27d3000c5d1ede6c15d0eefe94d939c269dd9
John Waldrep <johnwaldrep@gmail.com>
2015-08-01 07:13:39 -0400

    Hack FT runner to be able to test staging

--- pottytime/functional_tests/tests.py
+++ pottytime/functional_tests/tests.py
@@ -1,3 +1,5 @@
+import sys
+
 # from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
@@ -6,6 +8,19 @@ from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -24,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_log_a_potty_training_cycle(self):
         # Joan has heard about a cool new potty-training app to use with her toddler.
         # She visits the site with her potty-training son, Harold
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header are potty- and chart-centric
         self.assertIn('Potty', self.browser.title)
@@ -96,7 +111,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page.  There is no sign of Joan's chart
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('1: #0', page_text)
 
@@ -119,7 +134,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Joan goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
