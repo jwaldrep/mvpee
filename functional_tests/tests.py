37de93c4522c023a5a6e1920424515e2fb06a28f
John Waldrep <johnwaldrep@gmail.com>
2015-08-03 00:17:57 -0400

    Add user story for form validation

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 import unittest
+from unittest import skip
 from selenium.webdriver.common.keys import Keys
 
 
@@ -154,6 +155,23 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=5
         )
+
+    @skip
+    def test_cannot_add_empty_chart_items(self):
+        # Joan goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
+
+        # The home page refreshes, and there is an error message saying
+        # that chart items cannot be blank
+
+        # She tries again with an entry for the item, which now works
+
+        # Perversely, she now decides to submit a second blank chart item
+
+        # She receives a similar warning on the chart page
+
+        # And she can correct it by filling some text in
+        self.fail('write me!')
 
 if __name__ == '__main__':
     unittest.main()
